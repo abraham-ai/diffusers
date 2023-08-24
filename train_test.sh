3df276d27806f2034b5a0019121cb8439e8915a5
xander <xandersteenbruggex@gmail.com>
2023-08-24 11:48:44 -0700

    fix training with single img

--- train_test.sh
+++ train_test.sh
@@ -1,1 +1,1 @@
-cog predict -i lora_training_urls="https://storage.googleapis.com/public-assets-xander/Random/remove/banny/0.src.jpg|https://storage.googleapis.com/public-assets-xander/Random/remove/banny/5.src.jpg|https://storage.googleapis.com/public-assets-xander/Random/remove/banny/9.src.jpg" -i instance_prompt="a cartoon of Banny" -i name="Banny" -i max_train_steps=2
+cog predict -i lora_training_urls="https://storage.googleapis.com/public-assets-xander/Random/remove/banny/0.src.jpg" -i instance_prompt="a cartoon of Banny" -i name="Banny" -i max_train_steps=6
